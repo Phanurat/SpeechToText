76adb2a61c84b4933f22f4bbe30f912101498b0a
Phanurat <paunavt5@gmail.com>
2023-07-18 07:22:32 +0700

    Add Save to File.txt and Set time datetime

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import speech_recognition as sr
+from datetime import datetime
 
 recog = sr.Recognizer()
 
@@ -7,7 +8,7 @@ mic = sr.Microphone(device_index=1)
 
 # ใช้ recognizer เพื่อแปลงเสียงพูดเป็นข้อความ
 with mic as source:
-    miRe = 10
+    miRe = 3
     print("กำลังบันทึกเสียง",miRe,"วินาที...")
     audio = recog.record(source, duration=miRe)  # บันทึกเสียงเป็นเวลา 3 วินาที
 
@@ -15,6 +16,17 @@ with mic as source:
         # ใช้ Google Speech Recognition API แปลงเสียงเป็นข้อความภาษาไทย
         text = recog.recognize_google(audio, language='th')
         print("ข้อความที่ได้:", text)
+
+        #เวลาบันทึกในบัจจุบัน
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        current_date = now.strftime("%Y-%m-%d")
+        text_with_timestamp = f"{current_date} {current_time}: {text}"
+
+        # บันทึกลงในไฟล์ และ ทำให้เป็นภาษาไทยด้วย UTF-8
+        with open('speech.txt', 'a', encoding='utf-8') as file:
+                file.write(text_with_timestamp + '\n')
+
     except sr.UnknownValueError:
         print("ไม่สามารถรับรู้เสียง")
     except sr.RequestError as e:
